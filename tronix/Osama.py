8422241882df0e7a292ed76357e754a023937dba
MohammedHasan-99 <mo7ammed.mah@gmail.com>
2022-03-28 18:54:45 +0300

    messages between users

--- tronix/Osama.py
+++ tronix/Osama.py
@@ -7,8 +7,8 @@ ClientMultiSocket = socket.socket()
 a_lock = allocate_lock()
 
 host = '127.0.0.1'
-port = 2004
-id = '2'
+port = 2005
+name = 'Osama'
 UTF8 = 'utf-8'
 DASHES = '------------'
 
@@ -17,22 +17,26 @@ ClientMultiSocket.connect((host, port))
 # receive Server is working
 res = ClientMultiSocket.recv(1024)
 print(res.decode(UTF8))
-ClientMultiSocket.send(str.encode(id))
+ClientMultiSocket.send(str.encode(name))
+id = ClientMultiSocket.recv(1024).decode(UTF8)
+def thread_listener(connection):
+    while True:
+        r_msg = connection.recv(1024)
+        if not r_msg:
+            continue
+        r_msg = r_msg.decode('utf-8')
 
-# def thread_listener(connection):
-#     while True:
-#         r_msg = connection.recv(1024)
-#         if not r_msg:
-#             continue
-#         r_msg = r_msg.decode('utf-8').split('|')
-#         if r_msg[0] == 'msg':
-#             print(DASHES)
-#             print('notification message: ', r_msg[1])
-#             print(DASHES)
-#
-#
-# args = [ClientMultiSocket]
-# start_new_thread(thread_listener, tuple(args))
+        print(DASHES)
+        print('notification message: \n',r_msg)
+        print(DASHES)
+
+
+args = [ClientMultiSocket]
+start_new_thread(thread_listener, tuple(args))
+
+# def recive():
+#     return ClientMultiSocket.recv(1024)
+
 while True:
     print(
         'list of functionalities\n',
@@ -47,27 +51,26 @@ while True:
     if Input == OPTION.LIST_ONLINE_USERS.value:
         print('receiving online users from the server')
         # structure id | cmd
-        ClientMultiSocket.send(str.encode(id + '|' + str(OPTION.LIST_ONLINE_USERS.value)))
-        res = ClientMultiSocket.recv(1024)
-        print(DASHES)
-        print(res.decode(UTF8))
-        print(DASHES)
+        ClientMultiSocket.send(str.encode(str(id) + '|' + str(OPTION.LIST_ONLINE_USERS.value)))
+
     # option 2 is about sending to other user
     elif Input == OPTION.SEND_MESSAGE_TO_USER.value:
-        ClientMultiSocket.send(str.encode(id + '|' + str(OPTION.LIST_ONLINE_USERS.value)))
-        res = ClientMultiSocket.recv(1024)
-        print(DASHES, '\nonline users')
-        print(res.decode(UTF8))
-        print(DASHES)
-        Input = input('choose user id: ')
-        msg = input('write ur message: ')
-        # structure is [0] client id | [1] option asked | [2] id of the receiver client  | [3] message
-        ClientMultiSocket.send(str.encode(id + '|' + OPTION.SEND_MESSAGE_TO_USER.value + '|' + Input + '|' + msg))
-        print('message sent!!')
+        ClientMultiSocket.send(str.encode(str(id) + '|' + str(OPTION.LIST_ONLINE_USERS.value)))
+
+
+        reciver_id = input('choose user id: ')
+        while (True):
+            msg = input('write ur message: ')
+            if(msg == 'end'):
+                break
+            # structure is [0] client id | [1] option asked | [2] id of the receiver client  | [3] message
+            temp = str(id) + '|' + OPTION.SEND_MESSAGE_TO_USER.value + '|' + reciver_id + '|' + msg
+            ClientMultiSocket.send(str.encode(temp))
+            print('sent')
         print(DASHES)
     # option 6 will show messages
     elif Input == OPTION.SHOW_MESSAGES.value:
-        ClientMultiSocket.send(str.encode(id + '|' + OPTION.SHOW_MESSAGES.value))
+        ClientMultiSocket.send(str.encode(str(id) + '|' + OPTION.SHOW_MESSAGES.value))
         data = ClientMultiSocket.recv(1024)
         print(DASHES)
         print(data.decode(UTF8).split('|')[1])
@@ -78,3 +81,5 @@ while True:
         ClientMultiSocket.send(str.encode(id + '|' + OPTION.CLOSE_CONNECTION.value))
         sys.exit('closing the connection')
 ClientMultiSocket.close()
+
+
--- tronix/server.py
+++ tronix/server.py
@@ -4,9 +4,11 @@ from _thread import *
 
 ServerSideSocket = socket.socket()
 host = '127.0.0.1'
-port = 2004
-ThreadCount = 0
+port = 2005
+ThreadCount = -1
 UTF8 = 'utf-8'
+
+users = []
 
 ServerSideSocket.bind((host, port))
 print('Socket is listening..')
@@ -19,17 +21,9 @@ def multi_threaded_client(connection, add):
     # send to client Server is working
     connection.send(str.encode('Server is working:'))
     # wait for client to send his id
-    client_id = Client.recv(1024).decode(UTF8)
-    online_clients[client_id] = Client
-    # print('client id:', client_id)
-    # print here the online clients on the server each new connection
-    array_of_keys_in_server = []
-    online = ''
-    for k in online_clients.keys():
-        array_of_keys_in_server.append(k)
-    for i in range(len(array_of_keys_in_server)):
-        online = online + '\nonline id: ' + str(array_of_keys_in_server[i])
-    print(online)
+    client_name = connection.recv(1024).decode(UTF8)
+    users.append([ThreadCount,client_name,connection])
+    connection.send(str.encode(str(ThreadCount))) #send id to client
 
     # start chatting
     while True:
@@ -40,25 +34,22 @@ def multi_threaded_client(connection, add):
         arr = data.decode(UTF8).split('|')
         # split the request from the client
         # structure is [0] client id | [1] option asked
-        print(arr)
+        print(data.decode(UTF8))
         if arr[1] == OPTION.LIST_ONLINE_USERS.value:
             print('sending online users to client')
             array_of_keys = []
-            for k in online_clients.keys():
-                array_of_keys.append(k)
-            for i in range(len(array_of_keys)):
-                if array_of_keys[i] != arr[0]:
-                    response = response + '\nclient id: ' + str(array_of_keys[i])
-                else:
-                    response = response + '\nYOU!!'
-            connection.sendall(str.encode(response))
+            listOfUsers = ''
+            for user in users:
+                listOfUsers +=  'name: ' + user[1] +'    '+ 'id: ' + str(user[0]) + '\n'
+            connection.sendall(str.encode(listOfUsers))
 
         elif arr[1] == OPTION.SEND_MESSAGE_TO_USER.value:
             # client has chosen to send message to other client, so we have to get the other client connection from
             # the dictionary and send the message to it
             # structure is [0] client id | [1] option asked | [2] id of the receiver client  | [3] message
-            client = online_clients[arr[2]]
-            client.sendall(str.encode('msg' + '|' + arr[3]))
+            reciverCon = users[int(arr[2])][2]
+            message = 'Message from: ' + users[int(arr[0])][1] +'\nMessage content: '+ arr[3];
+            reciverCon.sendall(str.encode(message))
 
         elif arr[1] == OPTION.CLOSE_CONNECTION.value:
             print('client is asking to close the connection')
